1e4558242ad027d32bda4a3ac425a3252581391b
unknown <walterarzapalo23@gmail.com>
2022-11-11 01:24:30 -0500

    Agregar paginación (usando conteo, offset y limit de los elementos encontrados)

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,14 +1,24 @@
 const Product = require('../models/productModel');
 const { Op } = require("sequelize");    // Operadores de sequelize para consultas
 
+const ITEMS_PER_PAGE = 6;
+
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalProducts;
     const product_name = req.query.product_name;    // Request que tiene almacenado el dato de la busqueda en la URL
-    console.log(product_name);
     if (!product_name) {
         // Lista todos los productos si no existen datos en la busqueda
-        Product.findAll()
+        Product.findAndCountAll({
+            offset: (page - 1) * ITEMS_PER_PAGE,
+            limit: ITEMS_PER_PAGE
+        })
             .then(product => {
-                return res.status(200).json({ message: 'Productos listados exitosamente.', products: product });
+                totalProducts = product.count;
+                const prods = product.rows.map((res) => {
+                    return res.dataValues;
+                })
+                return res.status(200).json({ message: 'Productos listados exitosamente.', products: prods, totalProducts, ITEMS_PER_PAGE });
             })
             .catch(error => {
                 if (!error.statusCode) {
@@ -17,7 +27,9 @@ exports.getProducts = (req, res, next) => {
                 next(error);
             });
     } else {
-        Product.findAll({
+        Product.findAndCountAll({
+            offset: (page - 1) * ITEMS_PER_PAGE,
+            limit: ITEMS_PER_PAGE,
             where: {
                 name: {
                     [Op.substring]: product_name    // substring = %[texto]%
@@ -25,7 +37,11 @@ exports.getProducts = (req, res, next) => {
             }
         })
             .then(product => {
-                return res.status(200).json({ message: 'Productos listados exitosamente.', products: product });
+                totalProducts = product.count;
+                const prods = product.rows.map((res) => {
+                    return res.dataValues;
+                })
+                return res.status(200).json({ message: 'Productos listados exitosamente.', products: prods, totalProducts, ITEMS_PER_PAGE });
             })
             .catch(error => {
                 if (!error.statusCode) {
@@ -56,19 +72,27 @@ exports.getProductDetail = (req, res, next) => {
 };
 
 exports.getProductsByCategory = (req, res, next) => {
+    const page = +req.query.page || 1;
     const categoryId = req.params.categoryId;
-    Product.findAll({
+    let totalProducts;
+    Product.findAndCountAll({
+        offset: (page - 1) * ITEMS_PER_PAGE,
+        limit: ITEMS_PER_PAGE,
         where: {
             category: categoryId
         }
     })
         .then(products => {
+            totalProducts = products.count;
+            const prods = products.rows.map((res) => {
+                return res.dataValues;
+            })
             if (products.length === 0) {
                 const error = new Error('Error al listar productos.');
                 error.statusCode = 404;
                 throw error;
             }
-            return res.status(200).json({ message: 'Productos listados exitosamente.', products: products });
+            return res.status(200).json({ message: 'Productos listados exitosamente.', products: prods, totalProducts, ITEMS_PER_PAGE });
         })
         .catch(error => {
             if (!error.statusCode) {
