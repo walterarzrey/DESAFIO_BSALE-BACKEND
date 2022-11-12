b20e3219fdb4e2e55edb5b60b8b0cd6396665199
unknown <walterarzapalo23@gmail.com>
2022-11-12 13:19:24 -0500

    Modificar CORS

--- middleware/handlers.js
+++ middleware/handlers.js
@@ -12,5 +12,6 @@ exports.setHeaders = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Referrer-Policy', 'unsafe-url');
     next();
 }
