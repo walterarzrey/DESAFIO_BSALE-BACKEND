91099584bdbe54c5070e2c121505deb32cc59e84
unknown <walterarzapalo23@gmail.com>
2022-11-12 14:30:18 -0500

    deploy on heroku

--- utilities/dbConnection.js
+++ utilities/dbConnection.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('bsale_test', process.env.MYSQL_USER, 'bsale_test',{
+const db = new Sequelize('bsale_test', 'bsale_test', 'bsale_test',{
     dialect:'mysql',
-    host: process.env.HOST
+    host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com'
 });
 
 module.exports = db;
