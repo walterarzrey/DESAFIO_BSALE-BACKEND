208289b708510de9c5ba9235c2b986b90e76d37b
unknown <walterarzapalo23@gmail.com>
2022-11-12 19:27:56 -0500

    Modificar parámetros de envío para usar localStorage

--- utilities/dbConnection.js
+++ utilities/dbConnection.js
@@ -1,13 +1,8 @@
 const Sequelize = require('sequelize');
-/*
+
 const db = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD,{
     dialect:'mysql',
     host: process.env.HOST
-});*/
-
-const db = new Sequelize('bsale_test', 'bsale_test', 'bsale_test',{
-    dialect:'mysql',
-    host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com'
 });
 
 module.exports = db;
